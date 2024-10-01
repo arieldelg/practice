4f2c1574251327f5c86b1f2fed432d4d9018325e
arieldelg <arieldelgrande@hotmail.com>
2024-09-30 18:21:08 -0700

    adding styles to navlink Root.tsx

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -8,32 +8,44 @@ const Root = () => {
         <div className="w-full h-40">
           <img src={huron} alt="huron" className="w-full h-full object-cover" />
         </div>
-        <ul className="py-4">
-          <li className="w-full bg-slate-700 py-2 ">
+        <ul className="py-4 px-2">
+          <li className="w-full py-2 hover:bg-slate-500 rounded-xl hover:bg-opacity-50">
             <NavLink
               to={"/"}
               className={({ isActive, isPending }: NavLinkRenderProps) =>
-                isActive ? "nav-active" : isPending ? "nav-pending" : ""
+                isActive
+                  ? "nav-active"
+                  : isPending
+                  ? "nav-pending"
+                  : "inline-block w-full h-full"
               }
             >
               Home
             </NavLink>
           </li>
-          <li>
+          <li className="w-full py-2 hover:bg-slate-500 rounded-xl hover:bg-opacity-50">
             <NavLink
               to={"about"}
               className={({ isActive, isPending }: NavLinkRenderProps) =>
-                isActive ? "nav-active" : isPending ? "nav-pending" : ""
+                isActive
+                  ? "nav-active "
+                  : isPending
+                  ? "nav-pending"
+                  : "inline-block w-full h-full"
               }
             >
               About
             </NavLink>
           </li>
-          <li>
+          <li className="w-full py-2 hover:bg-slate-500 rounded-xl hover:bg-opacity-50">
             <NavLink
               to={"users"}
               className={({ isActive, isPending }: NavLinkRenderProps) =>
-                isActive ? "nav-active" : isPending ? "nav-pending" : ""
+                isActive
+                  ? "nav-active "
+                  : isPending
+                  ? "nav-pending "
+                  : "inline-block w-full h-full"
               }
             >
               users
