8d607e59743901538c2510d48830ff60557846e7
arieldelg <arieldelgrande@hotmail.com>
2024-10-03 09:27:02 -0700

    control props alt

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -7,50 +7,17 @@ const useShoppingCart = () => {
   }>({});
 
   const handleChange = (id: string, event: OnChangeProps) => {
-    switch (event.count) {
-      case -1:
-        if (shoppingCart[id]?.count === undefined) return;
-        if (shoppingCart[id]?.count - 1 === 0) {
-          delete shoppingCart[id];
-          return setShoppingCart((prev) => ({
-            ...prev,
-          }));
-        }
-        setShoppingCart((prev) => ({
-          ...prev,
-          [id]: {
-            product: event.product,
-            count: Math.max(shoppingCart[id]?.count - 1, 0) || 0,
-          },
-        }));
-        break;
-
-      case 1:
-        setShoppingCart((prev) => ({
-          ...prev,
-          [id]: {
-            product: event.product,
-            count: shoppingCart[id]?.count + 1 || 1,
-          },
-        }));
-        break;
-
-      default:
-        console.error("no match founded");
-        break;
-    }
-
     // ! metodo sencillo
-    // if (event.count === 0 && shoppingCart) {
-    //   delete shoppingCart[id];
-    //   return setShoppingCart((prev) => ({
-    //     ...prev,
-    //   }));
-    // }
-    // setShoppingCart((prev) => ({
-    //   ...prev,
-    //   [id]: event,
-    // }));
+    if (event.count === 0 && shoppingCart) {
+      delete shoppingCart[id];
+      return setShoppingCart((prev) => ({
+        ...prev,
+      }));
+    }
+    setShoppingCart((prev) => ({
+      ...prev,
+      [id]: event,
+    }));
   };
 
   return {
